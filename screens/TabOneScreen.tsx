608eeb0a03139ca8bcb4dbe39082346603ffa295
sybcreative <jamescoholan@sybcreative.com>
2021-02-21 23:01:44 -0800

    commit on 'main'

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,8 +3,7 @@ import { StyleSheet, Image, Button } from 'react-native';
 import { ScrollView, } from 'react-native-gesture-handler';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View,} from '../components/Themed';
-import { Audio } from 'expo-av';
-
+import { Audio,Video } from 'expo-av';
 
 export default function TabOneScreen() {
   const [sound, setSound] = React.useState();
@@ -29,21 +28,54 @@ export default function TabOneScreen() {
 
   return (
     
+    <ScrollView>
     <View style={styles.container}>
 
       <Image
         source={require('../assets/images/SYBLogo.png')}
         style={{
+          marginTop: 70,
           width: 200, 
           height: 60}}
       />
-      
+     
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
       <Button title="Play Song" onPress={playSound} />
+      <Video
+  source={{ uri: 'https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4' }}
+  rate={1.0}
+  volume={1.0}
+  isMuted={true}
+  resizeMode="cover"
+  shouldPlay
+  isLooping
+  style={{ width: 300, height:200, marginTop:40,marginBottom:50, }}
+/>
+<Video
+  source={{ uri: 'https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4' }}
+  rate={1.0}
+  volume={1.0}
+  isMuted={true}
+  resizeMode="cover"
+  shouldPlay
+  isLooping
+  style={{ width: 300, height:200, marginTop:40,marginBottom:50, }}
+/>
+<Video
+  source={{ uri: 'https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4' }}
+  rate={1.0}
+  volume={1.0}
+  isMuted={true}
+  resizeMode="cover"
+  shouldPlay
+  isLooping
+  style={{ width: 300, height:200, marginTop:40,marginBottom:50, }}
+/>
     </View>
+    </ScrollView>
   );
-
+ //http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4
 }
 
 const styles = StyleSheet.create({
