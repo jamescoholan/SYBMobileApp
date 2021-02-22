eafaf0dd0f3d81a4a549ea42bc2385dbd016af96
sybcreative <jamescoholan@sybcreative.com>
2021-02-22 11:43:14 -0800

    commit on 'main'

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
