ca88f59cebc5a66c6de835a7e542ad444f5a7e3e
sybcreative <jamescoholan@sybcreative.com>
2021-02-21 22:07:36 -0800

    commit on 'main'

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -13,7 +13,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          The SYB{'\n'}Mobile Experience
+          The SYB{'\n'}Mobile Experience!
         </Text>   
       </View>
 
