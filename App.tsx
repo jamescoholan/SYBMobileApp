b7849186c4c2f70622b25f2179b1b43f1c18c7b9
sybcreative <jamescoholan@sybcreative.com>
2021-02-22 19:06:55 -0800

    commit on 'main'

--- App.tsx
+++ App.tsx
@@ -6,6 +6,10 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+//Amplify.configure(awsconfig);
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
