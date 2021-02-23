9ef36806e5578cf23c199a895fa6c9796a2979e9
sybcreative <jamescoholan@sybcreative.com>
2021-02-23 11:33:07 -0800

    commit on 'main'

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,96 +1,113 @@
-import * as React from 'react';
-import { StyleSheet, Image, Button } from 'react-native';
-import { ScrollView, } from 'react-native-gesture-handler';
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View,} from '../components/Themed';
-import { Audio,Video } from 'expo-av';
+import * as React from "react";
+import { StyleSheet, Image, Button } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { Audio, Video } from "expo-av";
+import LottieView from 'lottie-react-native';
 
 export default function TabOneScreen() {
   const [sound, setSound] = React.useState();
-  
   async function playSound() {
-    console.log('Loading Sound');
+    console.log("Loading Sound");
     const { sound } = await Audio.Sound.createAsync(
-       require('../assets/audio/Paris_1.mp3')
+      require("../assets/audio/Paris_1.mp3")
     );
     setSound(sound);
 
-    console.log('Playing Sound');
-    await sound.playAsync(); }
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
 
   React.useEffect(() => {
     return sound
       ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync(); }
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
       : undefined;
   }, [sound]);
 
   return (
-    
     <ScrollView>
-    <View style={styles.container}>
+      <View style={styles.container}>
 
-      <Image
-        source={require('../assets/images/SYBLogo.png')}
-        style={{
-          marginTop: 70,
-          width: 200, 
-          height: 60}}
-      />
-     
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-      <Button title="Play Song" onPress={playSound} />
-      <Video
-  source={{ uri: 'https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4' }}
-  rate={1.0}
-  volume={1.0}
-  isMuted={true}
-  resizeMode="cover"
-  shouldPlay
-  isLooping
-  style={{ width: 300, height:200, marginTop:40,marginBottom:50, }}
-/>
-<Video
-  source={{ uri: 'https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4' }}
-  rate={1.0}
-  volume={1.0}
-  isMuted={true}
-  resizeMode="cover"
-  shouldPlay
-  isLooping
-  style={{ width: 300, height:200, marginTop:40,marginBottom:50, }}
-/>
-<Video
-  source={{ uri: 'https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4' }}
-  rate={1.0}
-  volume={1.0}
-  isMuted={true}
-  resizeMode="cover"
-  shouldPlay
-  isLooping
-  style={{ width: 300, height:200, marginTop:40,marginBottom:50, }}
-/>
-    </View>
+        <Image
+          source={require("../assets/images/SYBLogo.png")}
+          style={{
+            marginTop: 70,
+            width: 200,
+            height: 60,
+          }}
+        />
+
+        <View
+          style={styles.separator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+        <Button title="Play Song" onPress={playSound} />
+
+        <Video
+          source={{
+            uri:
+              "https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4",
+          }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={true}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={{ width: 300, height: 200, marginTop: 40, marginBottom: 50 }}
+        />
+
+        <Video
+          source={{
+            uri:
+              "https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4",
+          }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={true}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={{ width: 300, height: 200, marginTop: 40, marginBottom: 50 }}
+        />
+        <Video
+          source={{
+            uri:
+              "https://sybvideos.s3-us-west-1.amazonaws.com/Overtaken+Final+Jan+29+mp4.mp4",
+          }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={true}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={{ width: 300, height: 200, marginTop: 40, marginBottom: 50 }}
+        />
+      </View>
     </ScrollView>
   );
- //http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4
+  //http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 6,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 50,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,83 +1,84 @@
-import * as React from 'react';
-import { StyleSheet, Image, Button, ScrollView  } from 'react-native';
-import EditScreenInfo2 from '../components/EditScreenInfo2';
-import { Text, View, } from '../components/Themed';
+import * as React from "react";
+import { StyleSheet, Image, Button, ScrollView } from "react-native";
+import EditScreenInfo2 from "../components/EditScreenInfo2";
+import { Text, View } from "../components/Themed";
 
 export default function TabTwoScreen() {
   return (
-
     <ScrollView>
-    <View style={styles.container}>
-       <Image
-        source={require('../assets/images/SYBLogo.png')}
-        style={{width: 200, height: 60}}
-      />
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> 
-      <EditScreenInfo2 path="/screens/TabTwoScreen.tsx" />
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/SYBLogo.png")}
+          style={{ width: 200, height: 60 }}
+        />
+        <View
+          style={styles.separator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+        <EditScreenInfo2 path="/screens/TabTwoScreen.tsx" />
 
-      
-<Image
-        source={require('../assets/images/SYBIcon.png')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs01.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs00.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs02.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs03.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs04.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs05.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs06.jpg')}
-        style={{width: 400, height: 400}}
-      />
-<Image
-        source={require('../assets/images/SYBViz/PIIICs07.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs08.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs09.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs10.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs11.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs12.jpg')}
-        style={{width: 400, height: 400}}
-      />
-      <Image
-        source={require('../assets/images/SYBViz/PIIICs19.jpg')}
-        style={{width: 400, height: 400}}
-      />
-
-    </View>
+        <Image
+          source={require("../assets/images/SYBIcon.png")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs01.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs00.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs02.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs03.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs04.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs05.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs06.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs07.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs08.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs09.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs10.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs11.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs12.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+        <Image
+          source={require("../assets/images/SYBViz/PIIICs19.jpg")}
+          style={{ width: 400, height: 400 }}
+        />
+      </View>
     </ScrollView>
   );
 }
@@ -86,17 +87,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginVertical: 80,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 50,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  
+
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
