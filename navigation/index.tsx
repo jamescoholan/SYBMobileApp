587f8677e3d13e001e6a0cc97e0f1872fd9359bc
sybcreative <jamescoholan@sybcreative.com>
2021-02-22 22:53:54 -0800

    commit on'main'

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -8,6 +8,8 @@ import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 
+
+
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
